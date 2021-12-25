7dd6cd62481914a20790a37c95b28b925084f495
Makoji David <makojidavid@gmail.com>
2021-12-25 22:15:39 +0100

    concluded designs

---
+++ assets/main.js
@@ -0,0 +1,118 @@
+$(document).ready(function (){
+    let header = 
+    `
+    <nav class="top-nav">
+        <div class="nav-menu">
+            <div class="logo">
+                <h6 class="logo-text"><a href="./index.html" class="menu-links">Sassi Holford</a></h6>
+            </div>
+            <div class="top-menu">
+                <ul>
+                    <li><a href="" class="menu-links">About</a></li>
+                    <li><a href="#" class="menu-links">Bridal</a></li>
+                    <li><a href="#" class="menu-links">Fashion</a></li>
+                    <li><a href="#" class="menu-links">Appointments</a></li>
+                    <li><a href="#" class="menu-links">Enquiries</a></li>
+                    <li><a href="#" class="menu-links">Blog</a></li>
+                    <li><a href="./stores.html#" class="menu-links">Our Stores</a></li>
+                    <li><a href="#" class="menu-links"></a>Stockists</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="icons">
+            <ul>
+                <li><a href="#" class="menu-links">Account</a></li>
+                <li><img src="./assets/images/shop/search.png" alt="" class="search"></li>
+                <li><img src="./assets/images/shop/bag.png" alt="" class="cart"></li>
+            </ul>
+        </div>
+    </nav>
+    `
+
+    let  mobile_menu = 
+    `
+    <nav class="mobile-nav">
+        <div class="nav-items">
+            <div class="hamburger">
+                <img src="./assets/images/shop/menu.png" alt="hamburger" class="menu">
+            </div>
+            <div class="mobile-logo">
+                <h6 class="logo-text"><a href="./index.html" class="menu-links">Sassi Holford</a></h6>
+            </div>
+        </div>
+        <div class="mobile-icons">
+            <ul>
+                <li><a href="#" class="menu-links">Account</a></li>
+                <li><img src="./assets/images/shop/search.png" alt="" class="search"></li>
+                <li><img src="./assets/images/shop/bag.png"  alt="" class="cart"></li>
+            </ul>
+        </div>
+    </nav>
+    `
+    
+    let footer =
+    `
+    <div class="footer">
+        <div class="copyright">
+            <label for="copyright">&copy; 2021 sassi holford</label>
+            <ul>
+                <li><a href="#" class="footer-links">Terms & Conditions</a></li>
+                <li><a href="#" class="footer-links">Privacy policy</a></li>
+                <li><a href="#" class="footer-links">website by gc</a></li>
+            </ul>
+        </div>
+            <div class="footer-menu-lists">
+                <ul>
+                    <label for="about us">about us</label>
+                    <li><a href="#" class="footer-links">bridal stockists</a></li>
+                    <li><a href="#" class="footer-links">magazine</a></li>
+                    <li><a href="#" class="footer-links">our Story</a></li>
+                    <li><a href="#" class="footer-links">responsibility</a></li>
+                </ul>
+                <ul>
+                    <label for="about us">about us</label>
+                    <li><a href="#" class="footer-links">bridal stockists</a></li>
+                    <li><a href="#" class="footer-links">magazine</a></li>
+                    <li><a href="#" class="footer-links">our Story</a></li>
+                    <li><a href="#" class="footer-links">responsibility</a></li>
+                </ul>
+                <ul>
+                    <label for="about us">about us</label>
+                    <li><a href="#" class="footer-links">bridal stockists</a></li>
+                    <li><a href="#" class="footer-links">magazine</a></li>
+                    <li><a href="#" class="footer-links">our Story</a></li>
+                    <li><a href="#" class="footer-links">responsibility</a></li>
+                </ul>
+                <ul class="social-icons">
+                <li><img src="./assets/images/shop/facebook.png" alt=""></li>
+                <li><img src="./assets/images/shop/twitter.png" alt=""></li>
+                <li><img src="./assets/images/shop/instagram.png" alt=""></li>
+                <li><img src="./assets/images/shop/pinterest-circular-logo-symbol.png" alt=""></li>
+                <li><img src="./assets/images/shop/youtube.png" alt=""></li>
+                </ul>
+            </div>
+    </div>
+    `
+    $('.topNav').append(header);
+    $('.mobileNav').append(mobile_menu);
+    $('.footer').append(footer);
+    
+    // $('.menu-links').mouseover(function () { 
+    // $('.menu-links').classAdd('menu-links-animate').mouseOut(function(){
+    //     $('.menu-links').classRemove('menu-links-animate')
+    // }, 2500)
+// });
+});
+
+
+
+$(document).scrollTop( function (){
+    var offset = $('.topNav').offset()
+    var top = offset.top
+    if(scrollY >= top){
+        $('.topNav').addClass('sticky')
+    }else{
+        $('.topNav').removeClass('sticky')
+    }
+    
+});
