7adc8e4ef3ed78eb795888bb505236730c181fdd
Makoji David <57240208+aceDavon@users.noreply.github.com>
2021-12-26 15:12:53 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -22,8 +22,8 @@ $(document).ready(function (){
         <div class="icons">
             <ul>
                 <li><a href="#" class="menu-links">Account</a></li>
-                <li><img src="./assets/images/shop/search.png" alt="" class="search"></li>
-                <li><img src="./assets/images/shop/bag.png" alt="" class="cart"></li>
+                <li><img width="15px" height="15px" src="./assets/images/shop/search.png" alt="" class="search"></li>
+                <li><img width="15px" height="15px" src="./assets/images/shop/bag.png" alt="" class="cart"></li>
             </ul>
         </div>
     </nav>
