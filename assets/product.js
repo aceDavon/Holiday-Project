f30ff7a2233c19d353634d2a3717037f8f44cb5f
Makoji David <makojidavid@gmail.com>
2022-01-12 22:27:50 +0100

    extrated single product page to javascript

--- assets/product.js
+++ assets/product.js
@@ -73,5 +73,62 @@ $(document).ready(function () {
                 });
             } 
         });
+
+        $.ajax({
+            url: "https://fakestoreapi.com/products?limit=1",
+            method: "GET",
+            success: function(response) {
+                $(response).each(function(i, data){
+                    console.log(data)
+                    let product = `
+                    
+                    <div class="product-text">
+                    <div class="tablinks-col">
+                        <a href="#" class="tablinks">shop /</a>
+                        <a href="#" class="tablinks-inverted">seperated</a>
+                    </div>
+                    <div class="product-name">
+                        <p class="product-title">${data.title}</p>
+                        <p class="price">£${data.price}</p>
+                    </div>
+                    <div class="product-descript">
+                        <article>
+                            ${data.description}
+                            
+                        <div class="product-sizes">
+                            <a href="#" class="tablinks">34</a>
+                            <a href="#" class="tablinks">36</a>
+                            <a href="#" class="tablinks">38</a>
+                            <a href="#" class="tablinks">40</a>
+                            <a href="#" class="tablinks">42</a>
+                        </div>
+                        </article>
+                        <button class="article-btn-inverted" disabled="disabled">add to cart</button>
+        
+                        <p class="customer-care">
+                            SIZE HELP +
+                            <br>
+                            DELIVERIES & RETURNS +
+                            <br>
+                            ENQUIRIES +
+                        </p>
+                        <button class="article-btn-inverted">pin this</button>
+                    </div>
+                </div>
+                <div class="product-img">
+                    <div class="product1-img">
+                        <img src="./assets/images/SASSI-HOLFORD-RTW-SS21-AQUAREMARINE-PEARL-DUNSTER-TOP-DUNSTER-SHORTS-918x1366.jpg" alt="">
+                    </div>
+                    <div class="product2-img">
+                        <img src="${data.image}" alt="">
+                    </div>
+                </div>
+            </div>
+    
+                    `;
+                    $('.product-top-section').append(product)
+                });
+            } 
+        });
     
 });
